d8e7b53413852d9c69513fa7ff10e35b279d1c05
deytania9 <53503945+deytania9@users.noreply.github.com>
2024-11-07 16:30:09 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,18 +1,32 @@
 var buttonColours =["red","blue","green","yellow"];
-
+var userClickedPattern=[];
 function nextSequence(){
     var randomNumber=Math.floor(Math.random()*4);
-    console.log(randomNumber);
     var randomChosenColour=buttonColours[randomNumber];
-    console.log(randomChosenColour); 
     $(`#${randomChosenColour}`).click(function(){
         $(this).fadeOut(100).fadeIn(100);
-        var audio = new Audio(`./sounds/${randomChosenColour}.mp3`); 
-        
-        audio.play();
+        playSound(randomChosenColour);
+        animatePress( $(this));
+    })
+    $(`div[type="button"]`).click(function(){
+        var userChosenColour=$(this).attr("id");
+        userClickedPattern.push(userChosenColour);
+        playSound(userChosenColour);
+        animatePress($(this));
     })
 }
 
+function playSound(name){
+    var audio = new Audio(`./sounds/${name}.mp3`); 
+    audio.play();   
+}
+
+function animatePress(currentColour){
+    currentColour.addClass("pressed");
+    setTimeout(function() { 
+        currentColour.removeClass("pressed"); 
+    }, 100);
+}
 
 
 nextSequence()
